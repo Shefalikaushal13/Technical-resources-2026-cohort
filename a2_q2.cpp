8e42a2bc1f9dda630e84fb3cd36891e39e58f9b9
Shefali Kaushal <120242919+Shefalikaushal13@users.noreply.github.com>
2023-01-23 19:20:46 +0530

    Update and rename a1_q2.cpp to a2_q2.cpp
    Submittinng ques 2 assignment 2

---
+++ a2_q2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int n;
+ cout<<"Enter the number of lines you want in your pattern";
+ cin>>n;
+
+ for (int row= 1; row <= n; row++)
+ {
+     for (int col = 1; col <= 6-row; col++)
+       {
+         cout<<"* ";
+       }
+       cout<<endl;
+   }
+}
