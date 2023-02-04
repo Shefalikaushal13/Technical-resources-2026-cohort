9bb9fcd78f6d4667415402d540b547ddf4c2dd59
Shefali Kaushal <120242919+Shefalikaushal13@users.noreply.github.com>
2023-02-04 12:57:09 +0530

    Update a2_q5.cpp
    Submitting ques 5 off assign 2

--- a2_q5.cpp
+++ a2_q5.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 using namespace std;
 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int  i,j,k,space,n;
+   cout<<"Enter number of rows you want in the pattern: ";
+   cin>>n;
+   space = n - 1;
+   cout<<"\n";
+   
+   for  (i = 1; i <= n; i++)
+   {
+      for  (j = 1; j<= space; j++)
+         cout<<" ";
+         
+      space--;
+      for  (k = 1; k <= 2 * i - 1; k++)
+         cout<<"*";
+      cout<<"\n";
+   }
+   
+   space = 1;
+   for  (i = 1; i<= n - 1; i++)
+   {
+      for  (j = 1; j <= space; j++)
+         cout<<" ";
+      space++;
+      for  (k = 1 ; k <= 2 * (n - i) - 1; k++)
+         cout<<"*";
+      cout<<"\n";
+   }
+    
+    return 0;
+}
+
+
 int main(){
    
