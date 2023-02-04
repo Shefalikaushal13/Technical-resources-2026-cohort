b3da7df5343cab9bf32a1b5c8e3615650b4ce7e9
Shefali Kaushal <120242919+Shefalikaushal13@users.noreply.github.com>
2023-02-04 17:55:47 +0530

    Update a2_q6.cpp
    submitting ques 6 of assignment 2

--- a2_q6.cpp
+++ a2_q6.cpp
@@ -2,6 +2,27 @@
 using namespace std;
 
 int main(){
-   
+    int n;
+    cin>>n;
+    int sp=1,st=n/2;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=st+1;j++){
+           cout<<"*";
+        }
+        for(int j=1;j<=sp;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=st+1;j++){
+            cout<<"*";
+        }
+        if(i<=n/2){
+            st--;
+            sp=sp+2;
+        }else{
+            sp=sp-2;
+            st++;
+        }
+        cout<<endl;
+    }
     return 0;
 }
